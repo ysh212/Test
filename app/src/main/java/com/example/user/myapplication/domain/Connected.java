d7d5394401aed2995b7eec138e426f99d7035398
Hongvyo <30320525+Hongvyo@users.noreply.github.com>
2017-10-12 15:55:57 +0900

    Update Connected.java

--- app/src/main/java/com/example/user/myapplication/domain/Connected.java
+++ app/src/main/java/com/example/user/myapplication/domain/Connected.java
@@ -12,4 +12,22 @@ package com.example.user.myapplication.domain;
  */
 
 public class Connected {
+  //연결됬을 경우 코드
+  public static final int CONNECT = 1;
+  //연결해제됬을 경우 코드
+  public static final int DISCONNECT = 2;
+  //접수 혹은 등록이 되었을경우
+  public static final int REGISTER = 4;
+  //이거는 바꿀수도 있을거 같음 위랑 같은 내용이나 다름없어서
+  public static final int ACCEPT = 8;
+  //승인 거부된 경우
+  public static final int DENIED = 16;
+  //확정된 경우
+  public static final int CONFIRM = 32;
+  //취소된 경우
+  public static final int CANCEL = 64;
+  //상황이 종료된 경우 (변수 이름 바꿀 가능성이 높음)
+  public static final int CASE_CLOSED = 128;
+  //위치 업데이트인 경우
+  public static final int LOC_UPDATE = 256;
 }
